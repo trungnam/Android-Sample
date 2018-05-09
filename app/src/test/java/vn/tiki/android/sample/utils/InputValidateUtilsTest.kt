d5fd3a40001967d6748cb85319fea3cd38d323ca
trung nam <hjtman9922@gmail.com>
2018-05-09 19:02:57 +0700

    test code ultils

--- app/src/test/java/vn/tiki/android/sample/utils/InputValidateUtilsTest.kt
+++ app/src/test/java/vn/tiki/android/sample/utils/InputValidateUtilsTest.kt
@@ -1,8 +1,21 @@
 package vn.tiki.android.sample.utils
 
+import org.junit.Test
+
 /**
  * Created by trungnam1992 on 5/6/18.
  */
 class InputValidateUtilsTest{
     //to do
+    @Test
+    fun isPasswordValid() {
+    }
+
+    @Test
+    fun isEmailValid() {
+    }
+
+    @Test
+    fun isPhoneValid() {
+    }
 }
